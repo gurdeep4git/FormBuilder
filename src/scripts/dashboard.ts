35718eccb1db98160ab6c742745936ff7c95ccfe
Gurdeep Singh <gurdeep.singh@prmtechnologies.com>
2018-06-17 17:02:09 +0530

    Validations added

--- src/scripts/dashboard.ts
+++ src/scripts/dashboard.ts
@@ -34,22 +34,28 @@ export class Dashboard{
     }
 
     private addMoreField(){
-        this._fieldValue.id = Math.floor((Math.random() * 100) + 1);
-        this._fieldValue.title = Helper.getFieldTitle().val().toString().trim();
-        this._fieldValue.type =  Helper.getFieldType().val().toString().trim();
-        let listItem = `<li class='list-group-item d-flex justify-content-between' data-id='${this._fieldValue.id}'>
-                        <div>Title: <i>${this._fieldValue.title}</i>, Type: <i>${this._fieldValue.type}</i></div>
-                        <div><span class='fa fa-times js-remove-field'></span></div>
-                        </li>`;
-        Helper.getAddedaddedFieldsList().append(listItem);
-        Helper.getselectedFieldsArea().removeClass(this.dNone);
-        let  obj = {
-            id:this._fieldValue.id,
-            title:this._fieldValue.title,
-            type:this._fieldValue.type
-        };
-        this._formValue.FieldsList.push(obj);
-        this.clearFields();
+        if(Helper.getFieldTitle().val()!=""){
+            this._fieldValue.id = Math.floor((Math.random() * 100) + 1);
+            this._fieldValue.title = Helper.getFieldTitle().val().toString().trim();
+            this._fieldValue.type =  Helper.getFieldType().val().toString().trim();
+            let listItem = `<li class='list-group-item d-flex justify-content-between' data-id='${this._fieldValue.id}'>
+                            <div>Title: <i>${this._fieldValue.title}</i>, Type: <i>${this._fieldValue.type}</i></div>
+                            <div><span class='fa fa-times js-remove-field'></span></div>
+                            </li>`;
+            Helper.getAddedaddedFieldsList().append(listItem);
+            Helper.getselectedFieldsArea().removeClass(this.dNone);
+            let  obj = {
+                id:this._fieldValue.id,
+                title:this._fieldValue.title,
+                type:this._fieldValue.type
+            };
+            this._formValue.FieldsList.push(obj);
+            this.clearFields();
+        }
+        else{
+            $("#error-"+Helper.getFieldTitle()[0].id).show();
+            return false;
+        }
     }
 
     private clearFields(){
@@ -67,16 +73,36 @@ export class Dashboard{
     }
     
     private submitForm(){
-        this._formValue.title = Helper.getFormTitle().val().toString().trim();
-        this._formValue.layout = $("input[name="+this.LayoutRadio+"]:checked").val().toString().trim();
-        let formData = {
-            title:this._formValue.title,
-            layout:this._formValue.layout,
-            formFieldsList:this._formValue.FieldsList
+        if(Helper.getFormTitle().val()==""){
+            $("#error-"+Helper.getFormTitle()[0].id).show();
+            return false;
         }
-        let SendformData = JSON.stringify(formData);
-        window.sessionStorage.setItem("SendformData", SendformData);
-        location.href = "layout.html";
+        else if(!$("input[name="+this.LayoutRadio+"]").is(":checked")){
+            //@ts-ignore
+            let _a = $("input[name="+this.LayoutRadio+"]")[0].name;
+            $("#error-"+_a).show();
+            return false;
+        }
+        else if(this._formValue.FieldsList.length==0){
+            alert("Please select atleast one form field!!!");
+            return false;
+        }
+        // else if(Helper.getFormTitle().val()=="" && !$("input[name="+this.LayoutRadio+"]").prop("checked") && this._formValue.FieldsList.length==0){
+        //     alert("Please fill the form accordingly!!!");
+        //     return false;
+        // }
+        else{
+            this._formValue.title = Helper.getFormTitle().val().toString().trim();
+            this._formValue.layout = $("input[name="+this.LayoutRadio+"]:checked").val().toString().trim();
+            let formData = {
+                title:this._formValue.title,
+                layout:this._formValue.layout,
+                formFieldsList:this._formValue.FieldsList
+            }
+            let SendformData = JSON.stringify(formData);
+            window.sessionStorage.setItem("SendformData", SendformData);
+            location.href = "layout.html";
+        }
     }
 }
 new Dashboard();
